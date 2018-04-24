4a2dc99f2b56b7a7568b69409867dedd0c3552c2
mensinda <daniel@mensinger-ka.de>
2018-04-24 19:42:52 +0200

    initial commit

---
+++ __main__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+import enumGen
+import sys
+
+sys.exit(enumGen.main())
---
+++ enumGen.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import sys
+
+def main():
+  print("Hello World")
+
+if __name__ == '__main__':
+  sys.exit(main())
